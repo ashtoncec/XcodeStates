3d0a5b2007d1e21b288969bec5185978288f544e
ashtoncec <123290547+ashtoncec@users.noreply.github.com>
2024-03-25 12:07:24 -0500

    Initial Commit

---
+++ Ind03_Cecil_Ashton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind03_Cecil_Ashton
+//
+//  Created by Ashton Cecil on 3/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
