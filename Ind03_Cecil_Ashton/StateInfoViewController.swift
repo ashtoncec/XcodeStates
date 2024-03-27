e7e0c6397a743090fa45b51e6e8107a4104f32c1
ashtoncec <123290547+ashtoncec@users.noreply.github.com>
2024-03-26 23:37:11 -0500

    - Added 50 state flags and 50 state maps

---
+++ Ind03_Cecil_Ashton/StateInfoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StateInfoViewController.swift
+//  Ind03_Cecil_Ashton
+//
+//  Created by Ashton Cecil on 3/26/24.
+//
+
+import UIKit
+
+class StateInfoViewController: UIViewController {
+
+    @IBOutlet weak var StateLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Ind03_Cecil_Ashton/StatesTableViewController.swift
+++ Ind03_Cecil_Ashton/StatesTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class StatesTableViewController: UITableViewController {
     
+
+    
     let stateArray = [(state: "Alabama", nickname: "Yellowhammer State"), (state: "Alaska", nickname: "The Last Frontier"), (state: "Arizona", nickname: "The Grand Canyon State"), (state: "Arkansas", nickname: "The Natural State"), (state: "California", nickname: "The Golden State"), (state: "Colorado", nickname: "The Centennial State"), (state: "Connecticut", nickname: "The Constitution State"), (state: "Delaware", nickname: "The First State"), (state: "Florida", nickname: "The Sunshine State"), (state: "Georgia", nickname: "The Peach State"), (state: "Hawaii", nickname: "The Aloha State"), (state: "Idaho", nickname: "The Gem State"), (state: "Illinois", nickname: "The Prairie State"), (state: "Indiana", nickname: "The Hoosier State"), (state: "Iowa", nickname: "The Hawkeye State"),(state: "Kansas", nickname: "The Sunflower State"), (state: "Kentucky", nickname: "The Bluegrass State"), (state: "Louisiana", nickname: "The Pelican State"), (state: "Maine", nickname: "The Pine Tree State"), (state: "Maryland", nickname: "The Old Line State"), (state: "Massachusetts", nickname: "The Bay State"), (state: "Michigan", nickname: "The Great Lakes State"), (state: "Minnesota", nickname: "The North Star State"), (state: "Mississippi", nickname: "The Magnolia State"), (state: "Missouri", nickname: "The Show Me State"), (state: "Montana", nickname: "The Treasure State"), (state: "Nebraska", nickname: "The Cornhusker State"), (state: "Nevada", nickname: "The Silver State"), (state: "New Hampshire", nickname: "The Granite State"), (state: "New Jersey", nickname: "The Garden State"), (state: "New Mexico", nickname: "The Land of Enchantment"), (state: "New York", nickname: "The Empire State"), (state: "North Carolina", nickname: "The Tar Heel State"), (state: "North Dakota", nickname: "The Peace Garden State"), (state: "Ohio", nickname: "The Buckeye State"), (state: "Oklahoma", nickname: "The Sooner State"), (state: "Oregon", nickname: "The Beaver State"), (state: "Pennsylvania", nickname: "The Keystone State"), (state: "Rhode Island", nickname: "The Ocean State"), (state: "South Carolina", nickname: "The Palmetto State"), (state: "South Dakota", nickname: "Mount Rushmore State"), (state: "Tennessee", nickname: "The Volunteer State"), (state: "Texas", nickname: "The Lone Star State"), (state: "Utah", nickname: "The Beehive State"), (state: "Vermont", nickname: "The Green Mountain State"), (state: "Virginia", nickname: "The Old Dominion"), (state: "Washington", nickname: "The Evergreen State"), (state: "West Virginia", nickname: "The Mountain State"), (state: "Wisconsin", nickname: "The Badger State"), (state: "Wyoming", nickname: "The Equality State")]
     
     
@@ -53,6 +55,8 @@ class StatesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return stateHeader[0]
     }
+    
+   
     
     /*
     // Override to support conditional editing of the table view.
